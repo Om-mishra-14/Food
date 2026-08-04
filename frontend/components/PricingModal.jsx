7c67adee1deafc3bc3fde9e5dae7a51278d42ed6
om mishra <omrmishra2005@gmail.com>
2026-08-04 14:44:07 +0530

    fix: resolve PricingModal trigger rendering issue

--- frontend/components/PricingModal.jsx
+++ frontend/components/PricingModal.jsx
@@ -2,7 +2,6 @@
 import PricingSection from "./PricingSection";
 import {
   Dialog,
-  DialogTrigger,
   DialogContent,
   DialogTitle,
 } from "./ui/dialog";
@@ -14,13 +13,20 @@ const PricingModal = ({ subscriptionTier = "free", children }) => {
   const canOpen = subscriptionTier === "free";
 
   return (
-    <Dialog open={isOpen} onOpenChange={canOpen ? setIsOpen : undefined}>
-      <DialogTrigger render={children}></DialogTrigger>
-      <DialogContent className="p-6 sm:max-w-5xl max-h-[90vh] overflow-y-auto">
-        <DialogTitle className="sr-only">Upgrade to Pro</DialogTitle>
-        <PricingSection subscriptionTier={subscriptionTier} />
-      </DialogContent>
-    </Dialog>
+    <>
+      <span
+        onClick={() => canOpen && setIsOpen(true)}
+        style={{ cursor: canOpen ? "pointer" : "default" }}
+      >
+        {children}
+      </span>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent className="p-6 sm:max-w-5xl max-h-[90vh] overflow-y-auto">
+          <DialogTitle className="sr-only">Upgrade to Pro</DialogTitle>
+          <PricingSection subscriptionTier={subscriptionTier} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
